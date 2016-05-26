5d706a7a822da269f82a4f95db963f16f99b29ff
Everton Marques <everton.marques@gmail.com>
2016-05-26 07:08:01 -0300

    Fix decrement action.

--- example_test.go
+++ example_test.go
@@ -33,16 +33,16 @@ func ExampleGodux() {
 		case "INCREMENT":
 			return store.GetState("count").(int) + action.Value.(int)
 		case "DECREMENT":
-			return action.Value.(int) - store.GetState("count").(int)
+			return store.GetState("count").(int) - action.Value.(int)
 		default:
 			return store.GetAllState()
 		}
 	}
-	// Add your reducer function to return new values basend on your state
+	// Add your reducer function to return new values based on your state
 	store.Reducer(reducer)
 	// Receive new value
-	newCount := store.Dispatch(increment(10))
-	subCount := store.Dispatch(decrement(10))
+	newCount := store.Dispatch(increment(10)) // 1+10=11
+	subCount := store.Dispatch(decrement(10)) // 1-10=-9
 	fmt.Printf("Your Store state is: %d. Your newCount is: %d. Your subCount is: %d\n", store.GetState("count"), newCount, subCount)
-	// output: Your Store state is: 1. Your newCount is: 11. Your subCount is: 9
+	// output: Your Store state is: 1. Your newCount is: 11. Your subCount is: -9
 }
